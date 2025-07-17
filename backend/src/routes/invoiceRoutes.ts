e43b8321b14e5f2425ea0b698991d1825d16b7dd
Synaptiq <Main@synaptiq.bot>
2025-07-17 18:23:41 +0200

    nouveau proj

--- backend/src/routes/invoiceRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateInvoice } from "../controllers/invoiceController";
-
-const router = express.Router();
-
-router.post("/generate", generateInvoice);
-
-export default router;
